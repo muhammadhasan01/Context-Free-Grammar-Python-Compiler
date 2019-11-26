1ea57751b483bd0eab8bf0a8a048b6710c28aa80
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2019-11-26 22:08:00 +0700

    nambahin is

--- test.py
+++ test.py
@@ -1,10 +1,1 @@
-from PIL import Image
-import pytesseract
-import time
-import keyboard
-import random
-
-
-IMAGE_INPUT_NAME = 'input.jpg'
-FILE_OUTPUT_NAME = 'result.txt'
-ACCEPTED_ASCII_LIST = [x for x in range(32, 127)]
+print("oke")

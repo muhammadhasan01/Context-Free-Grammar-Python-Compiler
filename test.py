e2afc28677734b4f1c76c3ecc5db98349738c0e4
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2019-11-26 21:46:35 +0700

    Added Dictionary in Grammar

--- test.py
+++ test.py
@@ -1,15 +1,1 @@
-from PIL import Image
-import pytesseract
-import time
-import keyboard
-import random
-
-IMAGE_INPUT_NAME = 'input.jpg'
-FILE_OUTPUT_NAME = 'result.txt'
-<<<<<<< HEAD
-ACCEPTED_ASCII_LIST = []
-for x in range(32, 127):
-    ACCEPTED_ASCII_LIST.append(x)
-=======
-ACCEPTED_ASCII_LIST = [x for x in range(32, 127)]
->>>>>>> 7403bfc6c2f3985813e5bc962f07eec2be76b8d2
+ACCEPTED_ASCII_LIST = {a : b}

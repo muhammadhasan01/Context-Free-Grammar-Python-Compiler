651618f6f65a21448d9f401577b3206b12e07ccf
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2019-11-26 21:39:29 +0700

    edit minor bug grammar

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from PIL import Image
 import pytesseract
 import time
@@ -7,7 +6,6 @@ import random
 
 IMAGE_INPUT_NAME = 'input.jpg'
 FILE_OUTPUT_NAME = 'result.txt'
-ACCEPTED_ASCII_LIST = [x for x in range(32, 127)]
-=======
-string.printable
->>>>>>> 26ffeb730982b57c4281263ce94d1f0324357716
+ACCEPTED_ASCII_LIST = []
+for x in range(32, 127):
+    ACCEPTED_ASCII_LIST.append(x)

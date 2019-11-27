2ec44da07504d105fe747afd5ad5ec86113f7669
naufal-dean <naufal.dean@gmail.com>
2019-11-27 08:21:13 +0700

    Add bonus

--- main.py
+++ main.py
@@ -40,25 +40,37 @@ def preprocessInput(inp):
     newInp = re.sub("[xyz]{1}:[xyz]{1},", "", newInp)
     return (newInp + '\n')
 
+def highlightNameError(inp):
+    normal = "\033[1;37;40m"
+    red = "\033[1;37;41m"
+    newInp = ""
+    while inp:
+        x = re.search("[0-9]+[A-Za-z_]+", inp)
+        if x != None:
+            newInp += inp[:x.span()[0]] + red + x.group() + normal
+            inp = inp[x.span()[1]:]
+        else:
+            newInp += inp
+            inp = ""
+    return (newInp + '\n')
+
 def fileReader(path):
     with open(path, "r") as f:
         content = f.read()
     return content
 
 def banner():
-    print("   ___       _   _                        ")
-    print("  / _ \_   _| |_| |__   ___  _ __         ")
-    print(" / /_)/ | | | __| '_ \ / _ \| '_ \        ")
-    print("/ ___/| |_| | |_| | | | (_) | | | |       ")
-    print("\/     \__, |\__|_| |_|\___/|_| |_|       ")
-    print("       |___/                              ")
-    print("   ___                      _ _           ")
-    print("  / __\___  _ __ ___  _ __ (_) | ___ _ __ ")
-    print(" / /  / _ \| '_ ` _ \| '_ \| | |/ _ \ '__|")
-    print("/ /__| (_) | | | | | | |_) | | |  __/ |   ")
-    print("\____/\___/|_| |_| |_| .__/|_|_|\___|_|   ")
-    print("                     |_|                  ")
-    print("version 4.05")
+    print("                  ___       _   _                     ")
+    print("                 / _ \_   _| |_| |__   ___  _ __      ")
+    print("                / /_)/ | | | __| '_ \ / _ \| '_ \     ")
+    print("               / ___/| |_| | |_| | | | (_) | | | |    ")
+    print("               \/     \__, |\__|_| |_|\___/|_| |_|    ")
+    print("               ___    |___/             _ _           ")
+    print("              / __\___  _ __ ___  _ __ (_) | ___ _ __ ")
+    print("             / /  / _ \| '_ ` _ \| '_ \| | |/ _ \ '__|")
+    print("            / /__| (_) | | | | | | |_) | | |  __/ |   ")
+    print("            \____/\___/|_| |_| |_| .__/|_|_|\___|_|   ")
+    print("                                 |_|   version 4.05   \n\n")
 
 if __name__ == "__main__":
     # Get CNF
@@ -68,6 +80,7 @@ if __name__ == "__main__":
 
     # Input
     inp = fileReader(sys.argv[1])
+    inpHighlighted = highlightNameError(inp)
     source = inp
 
     # Preprocess
@@ -78,15 +91,14 @@ if __name__ == "__main__":
     print("Waiting for your verdict...\n")
 
     # Check
-    print("\n")
-    print(source)
-    print("\n")
-    print("===========")
+    print("========================= SOURCE CODE =========================\n")
+    print(inpHighlighted)
+    print("=========================== VERDICT ===========================\n")
     if (len(inp) == 0):
         print("Accepted")
-        print("===========")
+        print("\n===============================================================")
         exit(0)
-    
+
     # Parse
     CYKParser(inp, CNF)
-    print("===========")
+    print("\n===============================================================")
